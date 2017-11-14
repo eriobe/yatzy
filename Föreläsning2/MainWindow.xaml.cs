690dd60e4cf8e2e98e1b4cc17da01a14ef834c1e
eriobe <erik.oberg@miun.se>
2017-11-14 11:20:37 +0100

    FL2

--- Föreläsning2/MainWindow.xaml.cs
+++ Föreläsning2/MainWindow.xaml.cs
@@ -20,9 +20,84 @@ namespace Föreläsning2
     /// </summary>
     public partial class MainWindow : Window
     {
+        // globala variabler
+        string firstName;
         public MainWindow()
         {
             InitializeComponent();
+            txtValue.Text = "Fyll i ditt namn";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // skapar en variabel
+            // hårt typat programmeringsspråk
+
+            // namn Namn NAMn
+
+            // det finns olika datatyper
+
+            // datatyp variabelnamn plus eventuellt ett grundvärde
+
+            // deklarerar en variabel av datatypen string
+            string textboxValue;
+            string lastName = "Öberg";
+
+            // tilldelar variabeln ett värde från textboxen
+
+            //textboxValue = textboxValue + txtValue.Text;
+            //textboxValue = textboxValue + " Jag hoppas du mår bra";
+
+            textboxValue = txtValue.Text;
+
+
+            MessageBox.Show("Hej " + lastName + " " + txtValue.Text + " jag hoppas du mår bra.");
+            firstName = "Olle";
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            // variablers räckvidd
+            firstName = "Erik";
+            string lastName = "Äberg";
+            //typkonvertering
+
+            // gör om från en datatyp till en annan
+
+            // i det här fallet från en string till en int
+            int age = int.Parse(txtValue.Text);
+
+            // alla ljuger om sin ålder
+            age += 5;
+            MessageBox.Show(age.ToString());
+
+        }
+
+        private void btnKnapp_Click(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as Button).Content.ToString();
+            MessageBox.Show(content);
+        }
+
+        private void btnKnapp_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void whichDay(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnWeekday_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
